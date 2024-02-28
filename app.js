b56ed7d1e0d8a691dbc02fa84285d4d30e7b319f
cigs <x3irris@proton.me>
2024-02-28 21:30:24 +0000

    Fully functional

--- app.js
+++ app.js
@@ -5,12 +5,8 @@ document.addEventListener("DOMContentLoaded", function () {
 
 });
 
-
-
 function setupPage(){
-  const root = document.body;
-  const table = document.createElement("table");
-  table.id = "sudoku";
+  const table = document.getElementById("sudoku");
   let grid, cell;
   
   for (let i = 0; i < 9; i++){
@@ -19,12 +15,22 @@ function setupPage(){
     for(let j = 0; j < 9; j++){
 
       cell = document.createElement("td");
-      cell.className = "grid-cell c" + ((i*9)+j);
+      cell.className = "grid-cell";
       
       const input = document.createElement("input");
       input.type = "text";
       input.maxLength = "1";
-      input.className = "sudoku-input"; 
+      input.className = "sudoku-input";
+      input.id = "c" + ((i*9)+j);
+
+      input.addEventListener("input", () => {
+        const index = input.id.split('c')[1];
+        const row = index < size ? 0 : Math.floor(index / size);
+        const col = index < size ? index : index % size;
+        board[row][col] = input.value;
+
+      })
+
       cell.appendChild(input);
 
       grid.append(cell);
@@ -32,29 +38,106 @@ function setupPage(){
     table.append(grid);
   }
 
-  root.append(table)
-
+  generate(30);
 }
 
 
 const size = 9;
-let board, locked;
+let solution, locked, board;
 const randomNum = (n) => Math.floor(Math.random() * n);
 
 function generate(n) {
+  let sudoku = document.getElementById("sudoku");
+  let messageBox = document.getElementById("message-box");
+
+  sudoku.classList.remove("success", "fail");
+  messageBox.style.display = "none";
 
   solution = Array(size).fill().map(() => Array(size).fill(0));
+  board = Array(size).fill().map(() => Array(size).fill(0));
 
   locked = Array(81).fill(false);
 
+  //Helper function to create clues
   fillLocked(n, locked);
-  fillBoard(solution);
+  //Creates a valid solution
+  createFullBoard(solution);
+  //Creates a partiallly solved board
+  functionCaller(keepLocked, board);
+
+  functionCaller(updateView, board);
+}
+
+const solve = () => functionCaller(updateView, solution);
+const keepLocked = (index, i, j, array) => array[i][j] = locked[index] ? solution[i][j] : "";
+const updateView = (index, i, j, array) => {
+  document.getElementById("c" + index).value = array[i][j] == 0 ? "" : array[i][j];
+  board[i][j] = array[i][j] == 0 ? "" : array[i][j];
+}
+const validate = () => {
+  let cell, valid = true, index1D = 0;
+
+    for (let i = 0; i < size && valid; i++) {
+      for (let j = 0; j < size && valid; j++) {
+        index1D = i * 9 + j;
+        
+        valid = validateCell(i, j);
+  
+      }
+    }
+  
+  let sudoku = document.getElementById("sudoku");
+  let messageBox = document.getElementById("message-box");
+
+  sudoku.classList.remove("success", "fail");
+
+  if (valid) {
+    messageBox.textContent = "Sudoku is valid!";
+
+    sudoku.classList.add("success");
+
+  } else {
+    messageBox.textContent = "Sudoku is invalid";
+
+    sudoku.classList.add("fail");
+
+  }
+  messageBox.style.display = "block";
+
+};
+
+function validateCell(row, col){
+
+  const inRow = getInRow(board, row);
+  let inBox = Array();
+  let inCol = Array();
+  getInBox(board, row, col, inBox);
+  getInCol(board, col, inCol);
+
+  const cellValue = board[row][col];
+
+  const occurances = (arr, value) => arr.filter(item => item == value).length;
+
+  const occ = occurances(inRow, cellValue) + occurances(inCol, cellValue) + occurances(inBox, cellValue);
+
+  return occ == 3;
 
 }
 
-function solve() {}
+function functionCaller(func, param) {
+  
+  for (let i = 0; i < size; i++) {
+    for (let j = 0; j < size; j++) {
+      index1D = i * 9 + j;
+      
+      func(index1D, i, j, param);
 
-function fillBoard(boardArr) {
+    }
+  }
+}
+
+
+function createFullBoard(boardArr) {
   let row,
     col,
     nChoices,
@@ -89,26 +172,41 @@ function fillBoard(boardArr) {
     boardArr[row][col] = rolled;
   }
 }
+const ceilFunction = (num) => num - (num % 3);
 
-function getCellChoice(boardArr, row, col) {
-  const ceilFunction = (num) => num - (num % 3);
-  const checkRow = ceilFunction(row),
-    checkCol = ceilFunction(col);
+const getInRow = (arr, i) => arr[i].filter((n) => n > 0);
 
-  const inRow = boardArr[row].filter((n) => n > 0);
-  const inCol = Array();
-  boardArr.forEach((row) => {
+const getInCol = (arr, col, newArr) => {
+  arr.forEach((row) => {
     let num = row[col];
     if (num > 0) {
-      inCol.push(num);
+      newArr.push(num);
     }
   });
-  const inBox = Array();
+};
+
+const getInBox = (boardArr, row, col, inBox) => {
+  const checkRow = ceilFunction(row),
+  checkCol = ceilFunction(col);
+
   for (let i = checkRow; i < checkRow + 3; i++) {
     for (let j = checkCol; j < checkCol + 3; j++) {
       inBox.push(boardArr[i][j]);
     }
   }
+};
+const filterArray = (arr1, arr2) => arr1.filter((value) => arr2.includes(value));
+
+function getCellChoice(boardArr, row, col) {
+
+
+  const inRow = getInRow(boardArr, row);
+  const inCol = Array();
+  getInCol(boardArr, col, inCol);
+
+  const inBox = Array();
+  getInBox(boardArr, row, col, inBox);
+
 
   const fillFunction = (array) =>
     Array.from({ length: size }, (value, index) =>
@@ -119,8 +217,7 @@ function getCellChoice(boardArr, row, col) {
   const colChoices = fillFunction(inCol);
   const boxChoices = fillFunction(inBox);
 
-  const filterArray = (arr1, arr2) =>
-    arr1.filter((value) => arr2.includes(value));
+
   let choices = filterArray(rowChoices, colChoices);
   choices = filterArray(choices, boxChoices);
   while (choices.includes(0)) {
