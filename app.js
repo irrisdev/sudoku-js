d3fb96d9ed7769fc5edb02d012943140e027053b
cigs <x3irris@proton.me>
2024-02-28 17:01:42 +0000

    Generate function complete

--- app.js
+++ app.js
@@ -9,12 +9,12 @@ document.addEventListener("DOMContentLoaded", function () {
   let grid_item;
   let counter = 1;
 
-  for (let i = 1; i <= 9; i++) {
+  for (let i = 1; i <= size; i++) {
     sub_grid = document.createElement("div");
     sub_grid.className = "subgrid";
     sub_grid.id = "g" + i;
 
-    for (let j = 1; j <= 9; j++) {
+    for (let j = 1; j <= size; j++) {
       grid_item = document.createElement("input");
       grid_item.type = "text";
       grid_item.style.border = "none";
@@ -30,66 +30,108 @@ document.addEventListener("DOMContentLoaded", function () {
   }
 });
 
+const size = 9;
+const randomNum = (n) => Math.floor(Math.random() * n);
+let board, locked;
+
 generate(30);
 
 function generate(n) {
-  const board = Array(9)
+  board = Array(size)
     .fill()
-    .map(() => Array(9).fill(0));
-  const locked = Array(81).fill(false);
+    .map(() => Array(size).fill(0));
+  locked = Array(81).fill(false);
 
   fillLocked(n, locked);
   fillBoard(locked, board);
 
-  console.log(locked);
+
+
 }
 
 function solve() {}
 
+function fillBoard(boardArr) {
+  let row,
+    col,
+    nChoices,
+    rolled,
+    index = -1;
+  let choices = Array();
 
-function fillBoard(lockedArr, boardArr){
-  let row, col, nChoices, rolled, location, index = -1;
-  const choices = Array(81);
-  while(index < 80){
+  const get2Dindex = () => {
+    row = index < size ? 0 : Math.floor(index / size);
+    col = index < size ? index : index % size;
+  };
 
+  while (index < 80) {
     index++;
-    row = index < 9 ? 0 : Math.floor(index / 9);
-    col = index < 9 ? index : index % 9;
-
-    if(boardArr[row][col] == 0) {
+    get2Dindex();
+    if (boardArr[row][col] == 0) {
       choices[index] = getCellChoice(boardArr, row, col);
     }
 
     nChoices = choices[index].length;
-    console.log(nChoices);
-  }
 
+    while (nChoices == 0) {
+      index--;
+      nChoices = choices[index].length;
+      get2Dindex();
+      boardArr[row][col] = 0;
+    }
+    rolled = choices[index][randomNum(nChoices)];
+
+    choices[index].splice(choices[index].indexOf(rolled), 1);
+    
+    boardArr[row][col] = rolled;
+  }
 }
 
-function getCellChoice(boardArr, row, col){
+function getCellChoice(boardArr, row, col) {
   const ceilFunction = (num) => num - (num % 3);
-  let choices, rowChoices, colChoices, boxChoices, checkRow = ceilFunction(row), checkCol = ceilFunction(col);
-  
-  rowChoices = boardArr[row].filter((n) => n > 0);
-  colChoices = boardArr.filter((arr) => arr[col] > 0);
+  const checkRow = ceilFunction(row),
+    checkCol = ceilFunction(col);
 
-  boxChoices = new Array();
-  for (let i = checkRow; i < checkRow+3; i++){
-    for (let j = checkCol; j < checkCol+3; j++){
-      boxChoices.push(boardArr[i][j]);
+  const inRow = boardArr[row].filter((n) => n > 0);
+  const inCol = Array();
+  boardArr.forEach((row) => {
+    let num = row[col];
+    if (num > 0) {
+      inCol.push(num);
+    }
+  });
+  const inBox = Array();
+  for (let i = checkRow; i < checkRow + 3; i++) {
+    for (let j = checkCol; j < checkCol + 3; j++) {
+      inBox.push(boardArr[i][j]);
     }
   }
 
-  //TODO
+  const fillFunction = (array) =>
+    Array.from({ length: size }, (value, index) =>
+      array.includes(index + 1) ? 0 : index + 1
+    );
+
+  const rowChoices = fillFunction(inRow);
+  const colChoices = fillFunction(inCol);
+  const boxChoices = fillFunction(inBox);
+
+  const filterArray = (arr1, arr2) =>
+    arr1.filter((value) => arr2.includes(value));
+  let choices = filterArray(rowChoices, colChoices);
+  choices = filterArray(choices, boxChoices);
+  while (choices.includes(0)) {
+    choices.splice(choices.indexOf(0), 1);
+  }
   return choices;
 }
 
 function fillLocked(clues, locked) {
   for (let i = 0; i < clues; i++) {
-    let location = Math.floor(Math.random() * 81);
+    let location = randomNum(81);
 
     while (locked[location] == true) {
-      location = Math.floor(Math.random() * 81);
+      location = randomNum(81);
     }
     locked[location] = true;
   }
